9f62ba8405ea8131c556d91184dc21d60d4d6d0e
Grow Search <93290454+Growsearch@users.noreply.github.com>
2022-01-11 18:02:07 -0300

    fazendo alterações e acrescentando JS

---
+++ sitegs.js
@@ -0,0 +1,7 @@
+function porvir(){
+    window.alert("No momento estamos sem esta página. Trabalhando nisto!")
+}
+
+function carregar(){
+    window.alert("No momento ainda nao temos a prévia do e-book. Já estamos trabalhando nisto!")
+}

aaa526334b1834f4f1c6034a166bc54e256bcdc2
Grow Search <93290454+Growsearch@users.noreply.github.com>
2022-01-17 18:24:33 -0300

    adaptações

--- sitegs.js
+++ sitegs.js
@@ -1,5 +1,11 @@
 function porvir(){
     window.alert("No momento estamos sem esta página. Trabalhando nisto!")
+    var retorno = confirm('Quer ir para o BLOG?')
+    if (retorno == true) {
+        window.open ("https://growsearch.github.io/projeto-blog/index.html")
+    } else{
+        window.alert("voce preferiu ficar por aqui")
+    }
 }
 
 function carregar(){

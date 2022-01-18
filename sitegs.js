871c506144aab44ac1e7b155ed5f94f2e55aaff6
Grow Search <93290454+Growsearch@users.noreply.github.com>
2022-01-18 18:03:05 -0300

    adicionando interatividade em JS
    nesta sessão aprendi sobre transform,: rotate e translate em CSS3 e ainda adicionei algumas interatividades em JS

--- sitegs.js
+++ sitegs.js
@@ -11,3 +11,26 @@ function porvir(){
 function carregar(){
     window.alert("No momento ainda nao temos a prévia do e-book. Já estamos trabalhando nisto!")
 }
+
+// testando alguns incrementos em JS :)
+
+    let a = window.document.getElementById('anuncio-p')
+
+    a.addEventListener('mouseenter' , convidar)
+    a.addEventListener('mouseleave' , sair )
+    a.addEventListener('click' , clicou )
+
+    function convidar(){
+        a.innerText = "Quer conhecer melhor estas funções? clique aqui!"
+        a.style.background = "#fff"
+        a.style.fontSize = " 10px"
+    }
+
+    function sair(){
+        a.innerText = "Volte e clique se quiser conhecer as funções. :)"
+        a.style.background = "#44f187"
+    }
+
+    function clicou(){
+        a.click > alert("Parabéns por explorar a plataforma ao máximo, mas ainda estamos desenvolvendo algumas partes do site. Em breve, diversas novidades para vocês!")
+    }

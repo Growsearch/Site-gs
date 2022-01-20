c6880545c61dfef87ec9b2c3174f7ae5e4676581
Grow Search <93290454+Growsearch@users.noreply.github.com>
2022-01-20 19:04:31 -0300

    adicionando JS
    Hoje fiz um efeito de animação com Javascript e CSS animation. (maquina de escrever)

--- sitegs.js
+++ sitegs.js
@@ -12,25 +12,15 @@ function carregar(){
     window.alert("No momento ainda nao temos a prévia do e-book. Já estamos trabalhando nisto!")
 }
 
-// testando alguns incrementos em JS :)
+//treinando efeito de máquina de escrever 
 
-    let a = window.document.getElementById('anuncio-p')
+function typeWriter(elemento) {
+    const textoArray = elemento.innerHTML.split('');
+    elemento.innerHTML = '';
+    textoArray.forEach ((letra , i) => {
+        setTimeout(() => elemento.innerHTML += letra , 70 * i);
+    });
+}
 
-    a.addEventListener('mouseenter' , convidar)
-    a.addEventListener('mouseleave' , sair )
-    a.addEventListener('click' , clicou )
-
-    function convidar(){
-        a.innerText = "Quer conhecer melhor estas funções? clique aqui!"
-        a.style.background = "#fff"
-        a.style.fontSize = " 10px"
-    }
-
-    function sair(){
-        a.innerText = "Volte e clique se quiser conhecer as funções. :)"
-        a.style.background = "#44f187"
-    }
-
-    function clicou(){
-        a.click > alert("Parabéns por explorar a plataforma ao máximo, mas ainda estamos desenvolvendo algumas partes do site. Em breve, diversas novidades para vocês!")
-    }
+const titulo = document.querySelector('div.info > p')
+typeWriter(titulo);

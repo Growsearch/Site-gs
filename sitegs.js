1da5a2920be317d66f8cc7777260ea130aa3d6ff
Grow Search <93290454+Growsearch@users.noreply.github.com>
2022-01-26 17:33:11 -0300

    adicionando scroll em JS e Mudanças no design

--- sitegs.js
+++ sitegs.js
@@ -66,3 +66,44 @@ function toggleMenu(event){
 
 btnmobile.addEventListener('click' , toggleMenu)
 btnmobile.addEventListener('touchstart' , toggleMenu)
+
+
+// ANIMMANDO DATA-ANIME     SCROLL //
+
+function debounce(func, timeout = 200){
+    let timer;
+    return (...args) => {
+      clearTimeout(timer);
+      timer = setTimeout(() => { func.apply(this, args); }, timeout);
+    };
+  }
+  function saveInput(){
+    console.log('Saving data');
+  }
+  const processChange = debounce(() => saveInput());
+
+// FUNCTION DEBOUNCE copiei e colei da internet ----  site: https://www.freecodecamp.org/news/javascript-debounce-example/
+
+
+const target = document.querySelectorAll('[data-anime]');
+const animationClass = 'animate';
+
+function animeScroll() {
+    const windowTop  = window.pageYOffset + (window.innerHeight * 3 / 4);
+    target.forEach(function(element) {
+        if ((windowTop) > element.offsetTop) {
+            element.classList.add(animationClass);
+        } else{
+            element.classList.remove(animationClass);
+        }
+    })
+}
+
+animeScroll();
+
+if (target.length) {
+    window.addEventListener('scroll' , debounce(function() {
+        animeScroll();
+        console.log(dgdgshd)
+    } , 200));
+}

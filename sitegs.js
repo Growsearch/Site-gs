d2a83a37bb08b845c272efec927645dacb3cdfa4
Grow Search <93290454+Growsearch@users.noreply.github.com>
2022-02-07 17:50:42 -0300

    fazendo o menu mobile
    colocando menu mobile para a pagina de quem somos

--- sitegs.js
+++ sitegs.js
@@ -47,15 +47,15 @@ function clicou(){
     ativar.style.color = 'white'
 }
 
-// MENU MOBILE 
+// MENU MOBILE !
 
-let btnmobile = document.getElementById('btn-mobile');
+const btnmobile = document.querySelector('button.btn-mobile');
 
 function toggleMenu(event){
     if (event.type == 'touchstart') event.preventDefault()
-        let nav = document.getElementById('nav');
+        const nav = document.querySelector('nav.nav');
         nav.classList.toggle('active'); 
-        let active = nav.classList.contains('active');      
+        const active = nav.classList.contains('active');      
     event.currentTarget.setAttribute('aria-expanded' , 'true');
 
     if (active){ event.currentTarget.setAttribute('aria-label' , 'Fechar Menu')

83b51e2472453f602233d154858e621caa7d1a58
Grow Search <93290454+Growsearch@users.noreply.github.com>
2022-03-05 19:02:08 -0300

    atualizando o design do site
    atualizando a home mobile

--- sitegs.js
+++ sitegs.js
@@ -106,3 +106,28 @@ if (target.length) {
         animeScroll();
     } , 200));
 }
+
+
+    // PARTE DE CLICAR E APARECER A ESCRITA , NA PARTE DE CTA-EBOOK!!
+
+function initAccordion(){
+    const ebookLista = document.querySelectorAll('.ebook-list dt');
+    const activeClass = 'ativo';
+
+    if(ebookLista.length) {
+        ebookLista[0].classList.toggle(activeClass);
+        ebookLista[0].nextElementSibling.classList.toggle(activeClass);
+
+          function activeEbook() {
+             this.classList.toggle(activeClass);
+             this.nextElementSibling.classList.toggle(activeClass);
+            }
+
+        ebookLista.forEach((item) => {
+            item.addEventListener('click' , activeEbook);
+        });
+    }
+}
+
+initAccordion();
+        
